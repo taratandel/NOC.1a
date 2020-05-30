4eac10ad689c0694fa3e1dfc87d60917e82488e6
Tara Tandel <tara.tandel@icloud.com>
2020-05-30 11:20:18 +0200

    switch to sum

--- NOC1a.m
+++ NOC1a.m
@@ -17,15 +17,11 @@ SNR = 0;
 for i = 1 : length(sorted_amplitudes)
     SNR = SNR + sorted_amplitudes(i).^2 * p(i);
 end
-f(y) = for i = 1 : length(sorted_amplitudes)
-        p(a)*((1/sqrt(2*pi))*exp(-(7-Amplitudes(a)).^2/2);
-       end
-syms a
-symsum(p(a)*((1/sqrt(2*pi))*exp(-(7-Amplitudes(a)).^2/2)), a, 1, length(Amplitudes))
-
+% symsum(p(a)*((1/sqrt(2*pi))*exp(-(y-Amplitudes(a)).^2/2)), a, 1, length(Amplitudes))
+f_y = @(y) sum(p(a)*((1/sqrt(2*pi))*exp(-(y-Amplitudes(a)).^2/2)));
 % f_y = @(y) symsum(p(a).*(1/sqrt(2*pi)).*exp(-(y-Amplitudes(a)).^2/2), a, 1:length(Amplitudes));
-% fun = @(y) -f_y(y).*log2(f_y(y)+eps);
-% entY = integral(fun,-10,10);
+fun = @(y) -f_y(y).*log2(f_y(y)+eps);
+entY = integral(fun,-10,10);
 % entropy_of_noise = 1/2*log2(2*pi*exp(1));
 % 
-% mutual_information = entropy_of_noise - entY;
+mutual_information = entropy_of_noise - entY;
