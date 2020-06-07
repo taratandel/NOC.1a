a4630f2f7fd15d746b31bea55388d86f5ebc6849
Tara Tandel <tara.tandel@icloud.com>
2020-06-08 00:34:56 +0200

    plot the figure for different a

--- capacitytest.m
+++ capacitytest.m
@@ -3,10 +3,10 @@ clc, clear all
 M = 64;
 
 % vector of desired SNRs
-SNRdB_vec = 0:0.05:20;
+SNRdB_vec = 0:1:20;
 
 % I don't know what is this exaclty :))
-a_vectorized = linspace(0.25, 0.5068859522788, 30);
+a_vectorized = linspace(0.25, 0.5068859522788, 5);
 
 % offset for maxWell_boltzMan
 b = zeros(length(a_vectorized),1);
@@ -34,14 +34,29 @@ for m = 1:1:M
         p(m, j) = test(absolute_values_of_amplitudes(m),a) + b;
     end
 end
-% C = zeros(size(p));
-% j = 0;
-% for SNRdB = SNRdB_vec
-%     for i = 1:1:size(p,2)
-%         C(i) = QAMCapacity(SNRdB,x, p(:,i)');
-%     end
-%     break;
-% end
+C = zeros(size(SNRdB_vec,2),size(a_vectorized,2));
+j = 0;
+for SNRdB = SNRdB_vec
+    j = j+1;
+    for i = 1:1:size(p,2)
+        C(j,i) = QAMCapacity(SNRdB,x, p(:,i)');
+    end
+    
+end
+figure,
+xlabel('SNR [dB]')
+ylabel('C [bpcu]')
+legends = zeros(size(a_vectorized));
+for i = 1: 1: 5
+    c = C(:,i);
+    plot(SNRdB_vec, c, 'Linewidth', 2)
+
+    hold on, grid on
+    str = 'optimaly shaped';
+    out{i} = sprintf('%s_%d',str,i);
+    legend(out)
+end
+
 % for i = 1:1:M
 %     for a = a_vectorized
 %          + b;
@@ -68,12 +83,3 @@ end
 % end
 
 
-
-% figure,
-% plot(SNRdB_vec, C, 'Linewidth', 2)
-% xlabel('SNR [dB]')
-% ylabel('C [bpcu]')
-% hold on, grid on
-% plot(SNRdB_vec, C, 'r-', 'Linewidth', 2)
-% 
-% legend('Optimally shaped QAM')
