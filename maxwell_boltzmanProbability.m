1b8b495c761edf6112e095eb4b267fb81591eb5a
Tara Tandel <tara.tandel@icloud.com>
2020-06-11 10:35:10 +0200

    Change the name of the function
    will change the name of maxWell boltzman probability distribution from “test” to “max_bol_formula”

---
+++ maxwell_boltzmanProbability.m
@@ -0,0 +1,27 @@
+function P = maxwell_boltzmanProbability(M, no_amp, amp_dis, abs_amp)
+    % I don't know what is this exaclty :))
+    a_vectorized = linspace(0.1, 1.57, 9);
+    
+    p_opt = zeros(M,length(a_vectorized));
+    
+    no_of_amplitudes = no_amp;
+    
+    amplitude_distance = amp_dis;
+    
+    absolute_values_of_amplitudes = abs_amp;
+    for m = 1:1:M
+        j = 0;
+        for a = a_vectorized
+            j = j + 1;
+    %       offset is b in the graph
+            offset = 0;
+            for i = 1:1:length(no_of_amplitudes)
+                offset = offset + no_of_amplitudes(i)*test(amplitude_distance(i), a);
+            end
+            offset = 1/offset;
+
+            p_opt(m, j) = max_bol_formula(absolute_values_of_amplitudes(m),a).*offset;
+        end
+    end
+    P = p_opt;
+end
